ea6741c9c14d8f4520429ba0aafef92f88402277
jorgelozano25 <jorge_9dealmeria@hotmail.com>
2025-01-21 18:06:44 +0000

    terminado1

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 todos=[ 
     { "label": "My first task", "done": False } 
-    
+    { "label": "My second task", "done": False }
     ]
 
 @app.route('/todos', methods=['GET'])
